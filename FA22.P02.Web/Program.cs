0b9abdcfdab5d6b5e19495093e94fead32e67edd
oretha francis <francis.oretha@selu.edu>
2022-08-31 17:26:37 -0500

    check1

--- FA22.P02.Web/Program.cs
+++ FA22.P02.Web/Program.cs
@@ -38,10 +38,28 @@ app.MapGet("/weatherforecast", () =>
 
 app.Run();
 
+app.MapGet("/weatherforecast", () =>
+{
+    throw new Exception("remove me");
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateTime.Now.AddDays(index),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.Run();
+
 internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
+
 
 //see: https://docs.microsoft.com/en-us/aspnet/core/test/integration-tests?view=aspnetcore-6.0
 // Hi 383 - this is added so we can test our web project automatically. More on that later
